25800fe3231a0c897a48db0438a53863581eb647
Chad Kopelowitz <cmkopelowitz@byui.edu>
2020-06-15 21:34:53 -0600

    encoded all commands in cali_client.py

--- client/cali_client.py
+++ client/cali_client.py
@@ -38,91 +38,91 @@ def run(event):
 	global runbtn
 	print('motor ', runbtn)
 	if runbtn == 'Stop':
-		tcpCliSock.send('motor_stop')
+		tcpCliSock.send(b'motor_stop')
 		runbtn = 'Run'
 	elif runbtn == 'Run':
-		tcpCliSock.send('motor_run')
+		tcpCliSock.send(b'motor_run')
 		runbtn = 'Stop'
 
 def confirm(event):
-	tcpCliSock.send('confirm')
+	tcpCliSock.send(b'confirm')
 	
 	top.quit()
 	tcpCliSock.close()
 
 #--------motor---------------------
 def left_reverse(event):
-	left_cmd = 'leftreverse'
+	left_cmd = 'leftreverse'.encode()
 	tcpCliSock.send(left_cmd)
 
 def right_reverse(event):
-	right_cmd = 'rightreverse'
+	right_cmd = 'rightreverse'.encode()
 	tcpCliSock.send(right_cmd)
 #----------------------------------------
 
 #---------turing---------------
 def fineturn_left(event):
 	print('fineturn_left')
-	cmd = 'offset-1'
+	cmd = 'offset-1'.encode()
 	tcpCliSock.send(cmd)
 
 def fineturn_right(event):
 	print('fineturn_right')
-	cmd = 'offset+1'
+	cmd = 'offset+1'.encode()
 	tcpCliSock.send(cmd)
 
 def coarseturn_left(event):
 	print('coarseturn_left')
-	cmd = 'offset-10'
+	cmd = 'offset-10'.encode()
 	tcpCliSock.send(cmd)
 
 def coarseturn_right(event):
 	print('coarseturn_right')
-	cmd = 'offset+10'
+	cmd = 'offset+10'.encode()
 	tcpCliSock.send(cmd)
 #------------------------------
 
 #-----------mount-----------------
 #-------------x------------------
 def finex_left(event):
-	cmd = 'offsetx+1'
+	cmd = 'offsetx+1'.encode()
 	print(cmd)
 	tcpCliSock.send(cmd)
 
 def finex_right(event):
-	cmd = 'offsetx-1'
+	cmd = 'offsetx-1'.encode()
 	print(cmd)
 	tcpCliSock.send(cmd)
 
 def coarsex_left(event):
-	cmd = 'offsetx+10'
+	cmd = 'offsetx+10'.encode()
 	print(cmd)
 	tcpCliSock.send(cmd)
 
 def coarsex_right(event):
-	cmd = 'offsetx-10'
+	cmd = 'offsetx-10'.encode()
 	print(cmd)
 	tcpCliSock.send(cmd)
 
 #---------y-----------------------
 def finey_down(event):
 	print('finey_down')
-	cmd = 'offsety-1'
+	cmd = 'offsety-1'.encode()
 	tcpCliSock.send(cmd)
 
 def finey_up(event):
 	print('finey_up')
-	cmd = 'offsety+1'
+	cmd = 'offsety+1'.encode()
 	tcpCliSock.send(cmd)
 
 def coarsey_down(event):
 	print('coarsey_down')
-	cmd = 'offsety-10'
+	cmd = 'offsety-10'.encode()
 	tcpCliSock.send(cmd)
 
 def coarsey_up(event):
 	print('coarsey_up')
-	cmd = 'offsety+10'
+	cmd = 'offsety+10'.encode()
 	tcpCliSock.send(cmd)
 #--------------------------------
 
@@ -132,7 +132,7 @@ def coarsey_up(event):
 # =============================================================================
 def quit_fun(event):
 	top.quit()
-	tcpCliSock.send('motor_stop')
+	tcpCliSock.send(b'motor_stop')
 	tcpCliSock.close()
 
 # =============================================================================
